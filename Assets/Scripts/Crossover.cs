d001b52cd2210348c9641878851b2d9975e17d73
AmirKhodabakhshi1998 <62739801+AmirKhodabakhshi98@users.noreply.github.com>
2022-03-31 03:16:25 +0200

    1point crossover

--- Assets/Scripts/Crossover.cs
+++ Assets/Scripts/Crossover.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class Crossover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+
+    //takes 2 candidates and performs onepoint crossover. Returns unchanged candidates if random roll fails.
+    static (int[] child1, int[] child2) singlePointCrossover(int[] candidate1, int[]candidate2, float crossoverProbability)
     {
-        
+        float rndValue = Random.value;
+
+        //return unchanged candidates if random value isnt within crossoverProbability
+        if(rndValue > crossoverProbability)
+        {
+            return (candidate1, candidate2);
+        }
+
+
+        int length = candidate1.Length;
+
+
+        int crossoverPoint = Random.Range(1, length - 1); //end range is exclusive. Crossoverpoint cant be 0 or at last bit, because then children are copies of parents
+
+        //   int crossoverPoint2 = Random.Range(0, length); 
+
+
+        int[] child1 = candidate1;
+
+        for(int i=crossoverPoint; i<length; i++)
+        {
+            child1[i] = candidate2[i];
+        }
+
+
+        int[] child2 = candidate2;
+
+        for (int i=crossoverPoint; i<length; i++)
+        {
+            child2[i] = candidate1[i];
+        }
+
+
+        return (child1, child2);
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+    
+
+    
+
+
+
+
+
+
 }
