f3040f0b3a74010e489dc9fa5e47bf37574c8e7a
Adel Sabanovic <62741023+Adel-Sabanovic@users.noreply.github.com>
2022-04-01 12:31:42 +0200

    Mutation
    More changes

--- Assets/Scripts/Mutation.cs
+++ Assets/Scripts/Mutation.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Mutation : MonoBehaviour
 {
     private float mutationRate;
-    private Random r;
+    //private Random r;
 
       
 
     void mutate()
     {
-         r = new Random();
-        float randomValue = r.Next(0, 1);
+      
+        float randomValue = Random.Range(0,1);
 
         //  mutationRate = 1/(GeneticAlgorithm.RuleArray.length);
         // the bchance will be 1/512
