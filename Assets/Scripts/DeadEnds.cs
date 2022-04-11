096b826242346fd6f9312cd125ebcf7a0aab3399
Adel Sabanovic <62741023+Adel-Sabanovic@users.noreply.github.com>
2022-04-12 00:00:55 +0200

    Update DeadEnds.cs
    Updated döda slut

--- Assets/Scripts/DeadEnds.cs
+++ Assets/Scripts/DeadEnds.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class DeadEnds : MonoBehaviour
 {   
-    int shortestPath(int[,] level)
+    
+   public int deadEnds( int [,] level )
     {
-        LongestShortestPath.Node source = new LongestShortestPath.Node(0, 0, 0);
-
-        Queue<LongestShortestPath.Node> queue = new Queue<LongestShortestPath.Node>();
-        queue.Enqueue(new LongestShortestPath.Node(source.row, source.col, 0));
-
-        bool[,] visited = new bool[level.Length, level.Length];
-        visited[source.row, source.col] = true;
-
-        while(queue.Count != 0)
+        int score = 0;
+        int[,] start = new int[0, 0];
+        int destX;
+        int destY;
+        for (int col = 0; col < level.GetLength(0); col++)
         {
-
-        }
-
+            for (int row = 0; row < level.GetLength(1); row++)
+            {
+                destX = level[col, 0];
+                destY = level[0, row];
+                score += LongestShortestPath.shortestPath(level, destX, destY);
+                
+            }
+          
+        }return score;
     }
+
+}
